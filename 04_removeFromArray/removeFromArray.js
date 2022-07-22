c36b99009d892be54dc8087c2a741cf25c0b2620
Agung RB <agungrbudiman@gmail.com>
2022-07-23 01:02:26 +0700

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(items, ...values) {
+    let result = [];
+    for (const item of items) {
+        for (const value of values) {
+            if (item !== value) {
+                result.push();
+            }
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
