cf13a4cdbf3a9e9c71437bf875b790128f3010c6
Agung RB <agungrbudiman@gmail.com>
2022-07-26 13:42:51 +0700

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = parseInt(number);
+    if (number < 0) return 'OOPS';
+    fibSequence = Array(number);
+    for (let i = 0; i < fibSequence.length; i++) {
+        if (i <= 1) {
+            fibSequence[i] = 1;
+        }
+        else {
+            fibSequence[i] = fibSequence[i-1] + fibSequence[i-2];
+        }
+    }
+    return fibSequence[number-1];
 };
 
 // Do not edit below this line
