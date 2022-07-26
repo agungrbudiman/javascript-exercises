2110d5582d340a49c01bbf0cef96655aab4be6ea
Agung RB <agungrbudiman@gmail.com>
2022-07-26 12:48:44 +0700

    09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const normalizedString = string.toLowerCase().replace(/[^a-z0-9]+/g, '');
+    const reversedString = normalizedString.split('').reverse().join('');
+    return normalizedString == reversedString ? true : false;
 };
 
 // Do not edit below this line
