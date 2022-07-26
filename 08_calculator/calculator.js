6a77e6acaabcae34db70fcb1535899cbf0d20de0
Agung RB <agungrbudiman@gmail.com>
2022-07-26 10:59:50 +0700

    08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((total, number) => {
+    return total += number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total, number) => {
+    return total *= number;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+  let total = 1;
+  for (let i = 0; i < n2; i++) {
+    total *= n1;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(n1) {
+  let total = 1;
+  for (let i = 1; i <= n1; i++) {
+    total *= i
+  }
+  return total;
 };
 
 // Do not edit below this line
